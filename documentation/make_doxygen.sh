b46c7978056e4c61135edfc61014ac17600722a7
Jérémie Dudouet <j.dudouet@ipnl.in2p3.fr>
2023-08-09 10:17:37 +0200

    documentation update

--- documentation/make_doxygen.sh
+++ documentation/make_doxygen.sh
@@ -1,34 +1,34 @@
-DOXY_DIR=tkn.doxygen
-DOXY_FILE=tkn-dox-file.cfg
+DOXY_DIR=cubix.doxygen
+DOXY_FILE=cubix-dox-file.cfg
 
-if [[ -v TKN_SOURCE ]]; then
-echo $TKN_SOURCE
+if [[ -v CX_SOURCE ]]; then
+echo $CX_SOURCE
 else
-TKN_SOURCE=$PWD/..
-echo $TKN_SOURCE
+CX_SOURCE=$PWD/..
+echo $CX_SOURCE
 fi
 
 mkdir -p ./$DOXY_DIR
-cp $TKN_SOURCE/documentation/$DOXY_FILE ./
+cp $CX_SOURCE/documentation/$DOXY_FILE ./
 
-cp $TKN_SOURCE/documentation/css/* ./
-cp $TKN_SOURCE/documentation/js/* ./
-cp $TKN_SOURCE/documentation/layout/* ./
-cp -r $TKN_SOURCE/documentation/mdfiles $DOXY_DIR/
-cp -r $TKN_SOURCE/documentation/images $DOXY_DIR
-cp $TKN_SOURCE/icons/* $DOXY_DIR/images/
+cp $CX_SOURCE/documentation/css/* ./
+cp $CX_SOURCE/documentation/js/* ./
+cp $CX_SOURCE/documentation/layout/* ./
+cp -r $CX_SOURCE/documentation/mdfiles $DOXY_DIR/
+cp -r $CX_SOURCE/documentation/images $DOXY_DIR
+cp $CX_SOURCE/icons/* $DOXY_DIR/images/
 
 mkdir -p $DOXY_DIR/examples
-cp $TKN_SOURCE/examples/*.{cpp,C} $DOXY_DIR/examples
+cp $CX_SOURCE/examples/*.{cpp,C} $DOXY_DIR/examples
 
-cp -r $TKN_SOURCE/src $DOXY_DIR
-find tkn.doxygen/src \( -name '*.hpp' -o -name '*.json' -o -name '*LinkDef.h' -o -name '*.md'  -o -name '*.txt' -o -name '*.cmake' -o -name 'sqlite3' \) -exec rm -rf {} +
+cp -r $CX_SOURCE/src $DOXY_DIR
+find cubix.doxygen/src \( -name '*.hpp' -o -name '*.json' -o -name '*LinkDef.h' -o -name '*.md'  -o -name '*.txt' -o -name '*.cmake' -o -name 'sqlite3' -o -name 'tkn-lib' \) -exec rm -rf {} +
 
-MAJOR=`grep -m1 TkN_VERSION_MAJOR ${TKN_SOURCE}/CMakeLists.txt | cut -d\" -f2`
-MINOR=`grep -m1 TkN_VERSION_MINOR ${TKN_SOURCE}/CMakeLists.txt | cut -d\" -f2`
+MAJOR=`grep -m1 Cubix_VERSION_MAJOR ${CX_SOURCE}/CMakeLists.txt | cut -d\" -f2`
+MINOR=`grep -m1 Cubix_VERSION_MINOR ${CX_SOURCE}/CMakeLists.txt | cut -d\" -f2`
 
 PROJECT_NUMBER="${MAJOR}.${MINOR}"
-echo "TkN version: ${PROJECT_NUMBER}"
+echo "Cubix version: ${PROJECT_NUMBER}"
 
 echo "PROJECT_NUMBER = ${PROJECT_NUMBER}" >> $DOXY_FILE
 
